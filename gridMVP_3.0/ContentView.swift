2668187012b9f9c48d7c54400b1b5eb5b05564b5
Max Gilespie <max.gillespie10@gmail.com>
2019-09-26 14:39:07 -0500

    first commit

---
+++ gridMVP_3.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gridMVP_3.0
+//
+//  Created by Max Gillespie on 9/26/19.
+//  Copyright © 2019 Max Gillespie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
